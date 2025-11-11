060e52d70ff013ac09f51bf68795c120fcaaf292
ohuss049@uottawa.ca <ohuss049@uottawa.ca>
2025-11-11 18:43:24 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import plotly.graph_objects as go
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -11,3 +12,15 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def Temperature_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
+
+  def Temperature(self, **event_args):
+    """This method is called when the column panel is shown on the screen"""
+    pass
+
+  def plot_1_click(self, points, **event_args):
+    """This method is called when a data point is clicked."""
+    pass
